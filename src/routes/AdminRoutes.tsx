c90bbfe6dc470d9b665bcd58bcf8bcbad2b515f4
dehemiparamitha <dehemiparamitha21@gmail.com>
2026-02-19 17:44:52 +0530

    fix merge conflicts

--- src/routes/AdminRoutes.tsx
+++ src/routes/AdminRoutes.tsx
@@ -3,15 +3,9 @@ import { Routes, Route } from 'react-router-dom';
 import NotFound from '@/pages/NotFound';
 import { PaymentsAddingPage } from '@/pages/PaymentsAddingPage';
 import { PaymentsPage } from '@/pages/PaymentsPage';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { BankSlipReviewPage } from '@/pages/BankSlipReviewPage';
-=======
 import { BillingPage } from '@/pages/BillingPage';
->>>>>>> ae927e1 (Update payments page structure and routing)
-=======
-import { BankSlipReviewPage } from '@/pages/BankSlipReviewPage';
->>>>>>> 3514924 (Add initial bank slip review page)
+
 
 export const AdminRoutes: React.FC = () => {
   return (
@@ -19,16 +13,9 @@ export const AdminRoutes: React.FC = () => {
       <Route path="*" element={<NotFound />} />
       <Route path="payments" element={<PaymentsPage />} />
       <Route path="payments/customer/:customerId" element={<PaymentsAddingPage />} />
-<<<<<<< HEAD
-<<<<<<< HEAD
       <Route path="payments/slip/:slipId" element={<BankSlipReviewPage />} />
-=======
       <Route path="billing" element={<BillingPage />} />
-      
->>>>>>> ae927e1 (Update payments page structure and routing)
-=======
       <Route path="payments/slip/:slipId" element={<BankSlipReviewPage />} />
->>>>>>> 3514924 (Add initial bank slip review page)
     </Routes>
   );
 };
