67e9cfdba681b5da517ebf5d8eb640a86f1ede21
dehemiparamitha <dehemiparamitha21@gmail.com>
2026-04-25 15:42:32 +0530

    update service base URLs

--- src/services/customerService.ts
+++ src/services/customerService.ts
@@ -3,7 +3,7 @@ import { mockCustomers, mockCustomerBilling, Customer, CustomerBilling } from '@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "http://localhost:8081/api",
   headers: { "Content-Type": "application/json" },
 });
 
