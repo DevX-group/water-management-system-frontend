07e55b3ca6cdeaf0825b9c0064fbcfd8f79a53e0
kaweesha weerasinghe <weerasinghamkh.23@uom.lk>
2026-06-18 14:43:26 +0530

    add chatbot to all pages

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -3,6 +3,8 @@ import { ReactNode } from "react";
 import { Navbar } from "./Navbar";
 import { Footer } from "./Footer";
 
+import { CustomerChatbot } from "../chatbot/CustomerChatbot";
+
 interface MainLayoutProps {
   children: ReactNode;
   isAuthenticated?: boolean;
@@ -15,12 +17,15 @@ export const MainLayout = ({
   showFooter = true 
 }: MainLayoutProps) => {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col relative">
       <Navbar isAuthenticated={isAuthenticated} />
       <main className="flex-1 pt-16">
         {children}
       </main>
       {showFooter && <Footer />}
+      
+      {/* Floating Global Chatbot */}
+      <CustomerChatbot />
     </div>
   );
 };
