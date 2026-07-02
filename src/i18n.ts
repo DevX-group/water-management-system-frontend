528cea4e018a0687c35fe7053e84fc6a8913d276
dehemiparamitha <dehemiparamitha21@gmail.com>
2026-07-02 14:18:25 +0530

    feat: implement initial i18n for billing page

--- src/i18n.ts
+++ src/i18n.ts
@@ -4,6 +4,8 @@ import LanguageDetector from "i18next-browser-languagedetector";
 
 import enTranslation from "./locales/en/payments.json";
 import siTranslation from "./locales/si/payments.json";
+import enBilling from "./locales/en/billing.json";
+import siBilling from "./locales/si/billing.json";
 
 i18n
   .use(LanguageDetector)
@@ -12,9 +14,11 @@ i18n
     resources: {
       en: {
         translation: enTranslation,
+        billing: enBilling,
       },
       si: {
         translation: siTranslation,
+        billing: siBilling,
       },
     },
 
