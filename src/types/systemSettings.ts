fe350b4dd5c2c8b2522c35abb94487503019d374
dehemiparamitha <dehemiparamitha21@gmail.com>
2026-09-16 10:27:04 +0530

    feat: add overdue & disconnection policy settings to system settings

--- src/types/systemSettings.ts
+++ src/types/systemSettings.ts
@@ -1,23 +1,29 @@
-export interface SystemDetailsRequest {
-    companyName: string,
-    officeAddress: string,
-    officeContactNumber: string,
-    officeEmail: string,
-    defaultCurrency: string,
-    bankName: string,
-    branch: string,
-    accountNumber: string,
-    accountName: string,
+﻿export interface SystemDetailsRequest {
+    companyName: string;
+    officeAddress: string;
+    officeContactNumber: string;
+    officeEmail: string;
+    defaultCurrency: string;
+    bankName: string;
+    branch: string;
+    accountNumber: string;
+    accountName: string;
+    overdueThreshold?: number;
+    disconnectionGracePeriodDays?: number;
+    reconnectionFee?: number;
 }
 
 export interface SystemDetailsResponse {
-    companyName: string,
-    officeAddress: string,
-    officeContactNumber: string,
-    officeEmail: string,
-    defaultCurrency: string,
-    bankName: string,
-    branch: string,
-    accountNumber: string,
-    accountName: string,
+    companyName: string;
+    officeAddress: string;
+    officeContactNumber: string;
+    officeEmail: string;
+    defaultCurrency: string;
+    bankName: string;
+    branch: string;
+    accountNumber: string;
+    accountName: string;
+    overdueThreshold?: number;
+    disconnectionGracePeriodDays?: number;
+    reconnectionFee?: number;
 }
